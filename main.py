ecc5a782c8c1d43c0d8d9affe81e0106a0ff24a7
Sujan_Adhikari <079bct086@ioepc.edu.np>
2024-05-17 10:13:19 +0545

    displaying nearly done

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import socket
 import struct 
 import textwrap
+t1='\t - '
+t2='\t\t - '
+t3='\t\t\t - '
+t4='\t\t\t\t - '
 
+dt1='\t - '
+dt2='\t\t - '
+dt3='\t\t\t - '
+dt4='\t\t\t\t - '
 
 def main():
     conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
@@ -10,9 +18,20 @@ def main():
         raw_data, addr = conn.recvfrom(65536)
 
         dest_mac, src_mac, eth_proto, data =ethernet_frame(raw_data)
-        print("Ethernet frame\n")
-        print("Dest:{}, Souce:{}, Protocol:{}".format(dest_mac, src_mac, eth_proto))
+        print("\nEthernet frame")
+        print(t1+"Destination:{}, Souce:{}, Protocol:{}".format(dest_mac, src_mac, eth_proto))
 
+        #8 for ipv4
+        if eth_proto == 8:
+            (version, header_length, ttl, proto, src, target, data) =ipv4_packet(data)
+            print(t1+ 'IPv4 packet:')
+            print(t2+'version:{}, Header length: {}, ttl: {}'.format(version, header_length, ttl))
+            print(t2+'protocol:{}, source: {}, target: {}'.format(proto, src, target))
+            if proto == 1:
+                icmp_type, code, checksum, data =icmp_packet(data)
+                print(t1+ 'ICMP PACKET:')
+                print(t2+ 'TYPE:{}, CODE:{}')
+                print(t2+ 'ICMP PACKET:')
 
 
 # unpacking the ethernet frame:
@@ -29,4 +48,49 @@ def get_mac_addr(bytes_addr):
     mac_addr = ':'.join(bytes_str).upper() #formating in 09:7A:5R something like this
     return mac_addr
 
+#unpacking the Ipv4  packet
+def ipv4_packet(data):
+    version_header_length =data[0]
+    version = version_header_length>> 4
+    header_length =(version_header_length & 15) *4
+    ttl, proto, src, target =struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return version, header_length, ttl, proto, ipv4(src),ipv4(target), data[header_length:]
+
+#formating the ipv4
+def ipv4(addr):
+    return '.'.join(map(str,addr))
+
+
+# unpack ICmp packet
+def icmp_packet(data):
+    icmp_type, code, checksum = struct.unnpack('! B B H', data[:4])
+    return  icmp_type, code, checksum, data[4:]
+
+#unpacket tcp
+def tcp_segment(data):
+    (src_port, dest_port, sequence, acknowledgement, offset_reserved_flags) =struct.unpack('! H H L L H',data[:14])
+    #here we grabbbed the first 14 bytes of the data
+    offset = (offset_reserved_flags>>12)*4
+    flag_urg =(offset_reserved_flags & 32)>>5
+    flag_ack =(offset_reserved_flags & 16)>>4
+    flag_psh =(offset_reserved_flags & 8)>>3
+    flag_rst =(offset_reserved_flags & 4)>>2
+    flag_syn =(offset_reserved_flags & 2)>>1  
+    flag_fin =offset_reserved_flags & 1
+    return src_port, dest_port, sequence, acknowledgement,flag_urg, flag_ack, flag_psh,flag_rst, flag_syn, flag_fin, data[offset:]
+
+
+#unnpacking the UDP segment 
+def udp_segment(data):
+    src_port , dest_port, size =struct.unpack('! H H 2x H', data[:8])
+    return src_port, dest_port, size, data[:8]
+
+#formating the muli line data
+def format_multi_line(prefix, string, size=80):
+    size -= len(prefix)
+    if isinstance(string, bytes):
+        string= ''.join(r'\x{:02x}'.format(byte) for byte in string )
+        if size % 2 :
+            size-= 1
+        return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
 main()
